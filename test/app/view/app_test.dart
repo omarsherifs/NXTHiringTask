d3c831d3ba3b180708a0755f5c456304658dd089
Omar Sherif <omar.sherifs@live.com>
2024-04-24 22:27:52 +0200

    localization and test and packages

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nxthiringtask/app/app.dart';
-import 'package:nxthiringtask/counter/counter.dart';
+import 'package:nxthiringtask/features/main_user_screen/presentation/main_screen.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(MainScreen), findsOneWidget);
     });
   });
 }
