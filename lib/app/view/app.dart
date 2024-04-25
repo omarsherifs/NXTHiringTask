1600da8be1c988a4e55843a5e2e159cae0c33089
Omar Sherif <omar.sherifs@live.com>
2024-04-25 22:27:58 +0200

    add dark theme settings

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,22 +1,34 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:nxthiringtask/features/main_user_screen/presentation/main_screen.dart';
 import 'package:nxthiringtask/l10n/l10n.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({super.key, this.savedThemeMode});
+  final AdaptiveThemeMode? savedThemeMode;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
+    return AdaptiveTheme(
+      light: ThemeData(
         useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.blue,
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const MainScreen(),
+      dark: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.blue,
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        title: 'NXT Hiring Task',
+        theme: theme,
+        darkTheme: darkTheme,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: const MainScreen(),
+      ),
     );
   }
 }
