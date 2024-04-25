d4d20fe4fac710f2a14df5786fc7ce1a458905c6
Omar Sherif <omar.sherifs@live.com>
2024-04-25 22:28:04 +0200

    Update bootstrap.dart

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 import 'dart:developer';
 
-import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nxthiringtask/core/services/dependacny_injection_service.dart';
 
 class AppBlocObserver extends BlocObserver {
   const AppBlocObserver();
@@ -28,6 +29,7 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   Bloc.observer = const AppBlocObserver();
 
   // Add cross-flavor configuration here
+  DependencyInjectionServices().init();
 
   runApp(await builder());
 }
